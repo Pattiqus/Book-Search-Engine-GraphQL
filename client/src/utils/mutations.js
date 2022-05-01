1ecc5b1ed188af115fe910f578187dce541973d3
unknown <patticus.tv@gmail.com>
2022-05-01 20:38:44 +1000

    login and add user work

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,7 +13,7 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
+  mutation AddUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
       user {
@@ -22,7 +22,7 @@ export const ADD_USER = gql`
         email
         password
         savedBooks {
-          bookId
+          bookid
           authors
           description
           title
@@ -36,7 +36,7 @@ export const ADD_USER = gql`
 
 export const SAVE_BOOK = gql`
     mutation saveBook($bookId: ID!, $authors: [String], $description: String!, $title: String!, $image: String, $link: String ) {
-      saveBook(bookId: $bookId, authors: $authors, description: $description, title: $title, image:$image, link: $link) {
+      saveBook(bookid: $bookid, authors: $authors, description: $description, title: $title, image:$image, link: $link) {
         token
         user {
           _id
@@ -44,7 +44,7 @@ export const SAVE_BOOK = gql`
           email
           password
           savedBooks {
-            bookId
+            bookid
             authors
             description
             title
@@ -57,8 +57,8 @@ export const SAVE_BOOK = gql`
 `;
 
 export const REMOVE_BOOK = gql`
-  mutation removeBook($bookId: ID!) {
-      removeBook(bookId: $bookId ) {
+  mutation removeBook($bookid: ID!) {
+      removeBook(bookid: $bookid ) {
           token
           user {
               _id
@@ -66,7 +66,7 @@ export const REMOVE_BOOK = gql`
               email
               password
               savedBooks {
-                  bookId
+                  bookid
                   authors
                   description
                   title
